f14c63afc699ad5e9e806d801f97e6a1ebbe81c9
Euraxluo <euraxluo@outlook.com>
2023-12-10 09:53:34 +0000

    add tictactoe-dapp

---
+++ members/euraxluo/code/tictactoe-dapp/src/constants.tsx
@@ -0,0 +1,3 @@
+export const PACKAGE_ID = "0x1828137580fa60f0b6cec8a21965167759cbb00e3b373a58f624485e850bcfb0"
+export const STATE_ID = "0x8e48940fec292f96e0f18d54ea1c8c79e3d8bfc5445823ee3beaf953f9e39618"
+export const NET_ENV = "testnet"
