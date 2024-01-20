41547a87ed45bf9bac7129da20fbf6389248dfc9
uvd <386180127@qq.com>
2024-01-20 10:35:31 +0800

    Delete study directory study

--- study/app/source.ts
+++
@@ -1,7 +0,0 @@
-import { allDocs, allMeta } from 'contentlayer/generated'
-import { createContentlayer } from 'next-docs-zeta/contentlayer'
-
-export const { tree, getPage, getPageUrl } = createContentlayer(
-  allMeta,
-  allDocs
-)
--- study/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-export function randomAlphabet(str: string): string {
-  const randomAlphabet =
-    alphabets[Math.floor(Math.random() * alphabets.length)];
-
-  return str === " " ? " " : randomAlphabet ?? "";
-}
